4ce3a09e08d92b6d8e366de7b6202d36595feea9
Esha-Budram <402311115@my.richfield.ac.za>
2026-09-20 22:03:52 +0200

    creating crud in repository

--- app/PantryRepository.java
+++ app/PantryRepository.java
@@ -21,5 +21,56 @@ public class PantryRepository{
         db.close();
         return id;
     }
-    }uwujw
+    public List<PantryItem> getAllItems(){
+        List<PantryItem> items=new ArrayList<>();
+        SQLiteDatabase db=dbHelper.getReadableDatabase();
+
+        Cursor cursor=db.query(
+                PantryDBHelper.TABLE_ITEMS,null,null,null,null,null,PantryDBHelper.COL_EXPIRY+"ASC"
+        );
+        // Cursor works like a pointer moving through each row of the result
+        if (cursor.moveToFirst()) {
+            do {
+                PantryItem item = new PantryItem();
+                item.setId(cursor.getLong(cursor.getColumnIndexOrThrow(PantryDbHelper.COL_ID)));
+                item.setName(cursor.getString(cursor.getColumnIndexOrThrow(PantryDbHelper.COL_NAME)));
+                item.setCategory(cursor.getString(cursor.getColumnIndexOrThrow(PantryDbHelper.COL_CATEGORY)));
+                item.setQuantity(cursor.getInt(cursor.getColumnIndexOrThrow(PantryDbHelper.COL_QUANTITY)));
+                item.setUnit(cursor.getString(cursor.getColumnIndexOrThrow(PantryDbHelper.COL_UNIT)));
+                item.setExpiryDate(cursor.getString(cursor.getColumnIndexOrThrow(PantryDbHelper.COL_EXPIRY)));
+                items.add(item);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        // Updates an existing pantry item. The item's ID tells us which row to change.
+        public void updateItem(PantryItem item) {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            ContentValues values = new ContentValues();
+            values.put(PantryDbHelper.COL_NAME, item.getName());
+            values.put(PantryDbHelper.COL_CATEGORY, item.getCategory());
+            values.put(PantryDbHelper.COL_QUANTITY, item.getQuantity());
+            values.put(PantryDbHelper.COL_UNIT, item.getUnit());
+            values.put(PantryDbHelper.COL_EXPIRY, item.getExpiryDate());
+
+            //only update the row matching this item's ID
+            db.update(
+                    PantryDbHelper.TABLE_ITEMS,values,PantryDbHelper.COL_ID+" = ?",
+                    new String[]{String.valueOf(item.getId())}
+            );
+        }
+
+        // Deletes a pantry item by its ID
+        public void deleteItem(long id) {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+            db.delete(PantryDbHelper.TABLE_ITEMS,
+                    PantryDbHelper.COL_ID+" = ?",
+                    new String[]{String.valueOf(id)}
+            );
+        }
+        db.close();
+        return items;
+    }
 }
